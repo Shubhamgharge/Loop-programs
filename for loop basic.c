3f108a5cc30962b581aff955add10de48479107d
Shubhamgharge <159037288+Shubhamgharge@users.noreply.github.com>
2024-02-08 23:14:54 +0530

    Add files via upload

---
+++ for loop basic.c
@@ -0,0 +1,20 @@
+//for loop name 
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int i=0;
+   
+   for(i=1;i<=5;i++)
+   {
+      printf("\n%d",i);
+      printf("\nShubham");
+      printf("\nSambhaji");
+      printf("\nGharge");
+   }
+   printf("\n\nthanks");
+   
+   getch();
+   return 0;
+}
---
+++ while loop basic.c
@@ -0,0 +1,19 @@
+//while loop name
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+   int i=1;
+   
+   while(i<=5)
+   {
+      printf("\nshubham");
+      
+      i++;
+   }
+   printf("\n\nthanks");
+   
+   getch();
+   return 0;
+}
